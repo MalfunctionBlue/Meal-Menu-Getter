4242f7defda680478d03fe6bf928761291b0060e
David Yu <dyu25@ucsc.edu>
2018-05-02 00:26:32 -0700

    Everything should be normal....?

---
+++ src/Reader.java
@@ -0,0 +1,23 @@
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+
+import java.net.URL;
+
+
+public class Reader {
+    public static void pl(String line) {
+        System.out.println(line);
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url2 = "http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=College+Nine+%26+Ten&sName=&naFlag=";
+        String url="http://nutrition.sa.ucsc.edu/nutframe.asp?sName=UC+Santa+Cruz+Dining&locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&naFlag=1";
+        Document doc = Jsoup.connect(url).get();
+        Elements mainFrame=doc.select("Frame[name=AuroraMain]");
+        String mainFrameSrc=mainFrame.attr("src");
+        System.out.println(doc.text());
+        pl(mainFrame.toString());
+        pl(mainFrameSrc);
+    }
+}
