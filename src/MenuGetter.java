8bad48be655494ad6cb7dc312cb41b87e409bada
mzhang62 <mzhang62@ucsc.edu>
2018-05-02 01:47:00 -0700

    reconstructed MenuGetter

--- src/MenuGetter.java
+++ src/MenuGetter.java
@@ -13,19 +13,19 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class MenuGetter {
-    public static void main(String[] args) throws IOException {
+    private static Elements menuString;
+
+    public MenuGetter() throws IOException {
         String targetURL = "http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=College+Nine+%26+Ten&sName=&naFlag=";
         Document doc = Jsoup.connect(targetURL).get();
-        //System.out.println(doc.toString());
-        //"[class=menusamprecipes]" is the tag for food
-        Elements foods = doc.select("[class=menusamprecipes],[class=menusampmeals]");
-        //System.out.println(foods);
-
-        for (Element food : foods) {
-            System.out.println(food.text());
-        }
-
+        menuString = doc.select("[class=menusamprecipes],[class=menusampmeals]");
     }
+
+    public String toString() {
+        return menuString.text();
+    }
+
+
 }
 
 
